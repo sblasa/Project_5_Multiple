f42fbd0ba2f3ad62e8a2e7e7cce7100956721e74
sblasa <stblasa@gmail.com>
2017-07-20 19:26:49 -0700

    another practice

--- timestable.js
+++ timestable.js
@@ -1,13 +1,13 @@
-for (var i = 1; i <= 12; i++) {
-    console.log ("This is the " + i + "'s table.");
+for (var i =1; i <= 12; i++) {
 
-    for (var j = 0; j < 13; j++ ) {
-        console.log (i + "X" + j + " = " + i * j);
+	console.log("This is the " + i + "'s table.");
 
-    }
+	for(var j = 0; j<13; j++) {
+		console.log(i + "X" + j + " = " + i*j);
+	}
 
-    if(i < 12) {
-        console.log("________________");
+	if(i <12) {
+	console.log("_______");
+	}
 
-    }
 }
